847f33279c9752ac1221754a08a74af2fb8209ab
wangxianggan <382615666@qq.com>
2019-07-30 21:48:02 +0800

    a

--- 信安之路/index.php
+++ 信安之路/index.php
@@ -6,8 +6,17 @@
 </head>
 <body>
     <?php
-        $txt = 'world';
-        echo "hello $txt";
+        $con = mysql_connect('localhost', 'root', '_R123oo456t');
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        mysql_select_db('centos', $con);
+        $result = mysql_query('select * from people');
+        while ($row = mysql_fetch_array($result)) {
+            echo $row['id'] . '|' . $row['name'] . '|' . $row.age;
+            echo '<br />';
+        }
+        mysql_close($con);
     ?>
 </body>
 </html>
