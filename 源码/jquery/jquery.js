f168995f86963f3424521223edbf8f4cb02a6f59
王湘赣 <gana@wangxianggandeMacBook-Air.local>
2020-01-06 15:43:21 +0800

    a

--- 源码/jquery/jquery.js
+++ 源码/jquery/jquery.js
@@ -1,15 +1,24 @@
 function jquery(selector, context) {
 
-  // return new jquery.fn.init(selector, context)
+  return new jquery.fn.init(selector, context)
 }
 
 jquery.fn = jquery.prototype = {
-  version: '0.0.1'
+  version: '0.0.1',
+  init () {
+    console.log(222)
+    return this
+  }
 }
 
+
 jquery.fn.ready = function (fn) {
+  console.log(111)
   return this
 }
+
+jquery.fn.init.prototype = jquery.fn
+
 
 window.jquery = window.$ = jquery
 
