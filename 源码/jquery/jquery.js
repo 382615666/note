068424391f5ab5736b755de25c9fc8eb429cc532
wangxianggan <382615666@qq.com>
2020-05-28 00:02:46 +0800

    a

--- 源码/jquery/jquery.js
+++ 源码/jquery/jquery.js
@@ -5,7 +5,13 @@ function jquery (selector, context) {
 jquery.fn = jquery.prototype = {
   jquery: '1.0.0',
   construtor: jquery,
-  length: 0
+  length: 0,
+  init () {
+    return this
+  },
+  toArray () {
+    return [].slice.call(this)
+  }
 }
 
 export default jquery
